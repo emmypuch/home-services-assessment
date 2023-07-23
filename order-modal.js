259e407a6b439f038f1f83320dbcc0ff650739b8
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-07-23 01:12:40 +0100

    wip

---
+++ order-modal.js
@@ -0,0 +1,24 @@
+// Function to show the modal with the item and status information
+function showModalWithContent(item, status) {
+  const modalContent = document.querySelector(".modal-content");
+  modalContent.innerHTML = `
+        <h2>Order Status</h2>
+        <p>Your ${item} order is ${status}.</p>
+        <button id="close-modal">Close</button>
+    `;
+  modal.style.display = "flex";
+}
+
+// Get the data from localStorage
+const item = localStorage.getItem("item");
+const status = localStorage.getItem("status");
+
+// Show the modal with the content if data is present
+if (item && status) {
+  showModalWithContent(item, status);
+}
+
+// Close the modal when the close button is clicked
+document.getElementById("close-modal").addEventListener("click", () => {
+  modal.style.display = "none";
+});
